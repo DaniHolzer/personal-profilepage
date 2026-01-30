99dc0ff8c38fd7c0f94bbc87786789b1d13d2443
DaniHolzer <holzerdaniel01@gmail.com>
2026-01-30 17:47:40 +0100

    Style root-Home for mobile

---
+++ src/layouts/Heading.tsx
@@ -0,0 +1,10 @@
+
+
+export function Heading() {
+  return (
+    <section className="flex justify-between p-2.5 w-full">
+      <p>Daniel Holzer</p>
+      <button>nav</button>
+    </section>   
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import { Heading } from "../layouts/Heading.tsx";
+
+export function Home() {
+  return (
+    <Heading />
+    
+  );
+}
