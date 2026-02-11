58d94e212dfa54db0c627dc42dc9b9108b51b69c
DaniHolzer <holzerdaniel01@gmail.com>
2026-02-11 11:37:22 +0100

    Remove deprecated components and layouts.

--- src/layouts/layout/Layout.styles.ts
+++
@@ -1,8 +0,0 @@
-import { tv } from 'tailwind-variants';
-
-//-----------------------------------------------------------------------------
-// Layout - Styles
-
-export const layoutStyles = tv({
-  base: 'bg-red-600 flex flex-col justify-start items-center mx-auto my-0 min-h-screen',
-});
