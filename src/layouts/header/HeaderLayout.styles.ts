691dfb1fbbeee2f661cefe1dff7208ca8ee59c9b
DaniHolzer <holzerdaniel01@gmail.com>
2026-02-14 11:06:14 +0100

    Implement design layout in Header.

--- src/layouts/header/HeaderLayout.styles.ts
+++ src/layouts/header/HeaderLayout.styles.ts
@@ -4,7 +4,7 @@ import { tv } from "tailwind-variants";
 // HeaderLayout Styles
 
 export const headerLayoutStyles = tv({
-  base: "fixed top-0 left-0 right-0 h-14",
+  base: "fixed top-0 left-0 right-0 h-14 flex justify-between items-center p-[1rem] h-auto",
   variants: {
     debug: {
       true: "border-2 border-dashed border-blue-500 bg-blue-300",
--- src/layouts/header/HeaderLayout.tsx
+++ src/layouts/header/HeaderLayout.tsx
@@ -1,5 +1,7 @@
 import { headerLayoutStyles } from "./HeaderLayout.styles";
 import type { HeaderLayoutProps } from "./HeaderLayout.types";
+import { Link } from "components/links/base/Link";
+import { HotdogButton } from "components/button/variants/hotdogButton/HotdogButton";
 
 //-------------------------------------------------------------------------------------------------
 // HeaderLayout - Component
@@ -8,7 +10,15 @@ export function HeaderLayout({ className, debug }: HeaderLayoutProps) {
 
   return (
     <header id="header" className={headerLayoutStyles({ className, debug })}>
-      I'm the header
+      <Link
+        id="header__logo"
+        to="#"
+        className="text-[2.4rem] underline underline-offset-4 decoration-red-500"
+        children="Daniel Holzer"
+        aria-label="Logo"
+        title="Home"
+      />
+      <HotdogButton />
     </header>
   );
 }
